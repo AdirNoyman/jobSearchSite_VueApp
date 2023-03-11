3c5c860a9090096c61364e533d27083b2b36470d
AdirNoyman <ofahol2007@hotmail.com>
2023-03-11 19:56:02 +0200

    Added header components 1

--- job-search/src/App.vue
+++ job-search/src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1 class="mt-8 text-6xl text-center">Hello Adiros 🤓</h1>
+  <MainNav />
 </template>
+
+<script>
+import MainNav from '@/components/MainNav.vue'
+export default {
+  name: 'App',
+  components: {
+    MainNav
+  }
+}
+</script>
--- job-search/tailwind.config.js
+++ job-search/tailwind.config.js
@@ -5,8 +5,15 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts}'],
   theme: {
     extend: {
+      // Adding my own fonts to Tailwind
       fontFamily: {
         sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
+      },
+      // Adding my own colors to Tailwind
+      colors: {
+        'brand-gray-1': '#dadce0',
+        'brand-blue-1': '#1967d2',
+        'brand-green-1': '#137333'
       }
     }
   },
