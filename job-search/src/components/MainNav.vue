7a2fa364516638a48ecabf286ca261a0a25b933a
AdirNoyman <ofahol2007@hotmail.com>
2023-03-13 22:58:39 +0200

    started adding unit tests

--- job-search/src/components/MainNav.vue
+++ job-search/src/components/MainNav.vue
@@ -2,7 +2,10 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <a href="/" class="flex h-full items-center text-xl">Adirs Careers</a>
+        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <h2 class="ml-8 flex h-full items-center">
+          Developed by {{ author.firstName }} {{ author.lastName }}
+        </h2>
       </div>
     </div>
   </header>
@@ -11,7 +14,16 @@
 <script>
 export default {
   // The name of the Vue component
-  name: 'MainNav'
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'Adiros Careers',
+      author: {
+        firstName: 'Adiros',
+        lastName: 'Gadolos'
+      }
+    }
+  }
 }
 </script>
 
---
+++ job-search/tests/unit/playground.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest'
+
+describe('describe = Describes what we are testing', () => {
+  it('add two numbers', () => {
+    // expect = assertion
+    // toBe = expected result
+    expect(1 + 1).toBe(2)
+  })
+})
